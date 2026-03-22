c8e8671523f949ef29f5f440924de81e021d1171
Артём Ковалев <artiomkratov@gmail.com>
2026-03-22 11:32:29 +0300

    feat: added babel and PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from 'vite';
+import babel from 'vite-plugin-babel';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   server: {
@@ -9,4 +11,23 @@ export default defineConfig({
     target: 'esnext',
     outDir: 'dist',
   },
+  plugins: [
+    babel(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,hbs}']
+      },
+      manifest: {
+        name: 'NeXus SPA',
+        short_name: 'NeXus',
+        theme_color: '#0a0a0a',
+        background_color: '#0a0a0a',
+        display: 'standalone',
+        icons: [
+          { src: '/public/favicon.ico', sizes: '192x192', type: 'image/x-icon' }
+        ]
+      }
+    })
+  ]
 });
