a408f062820927059817880c063f34c0f0e3d7fb
Артём Ковалев <artiomkratov@gmail.com>
2026-05-21 23:35:26 +0300

    fix: edit button icon

--- src/modules/kanban/KanbanView.ts
+++ src/modules/kanban/KanbanView.ts
@@ -784,7 +784,6 @@ export class KanbanView {
                   <polyline points="15 3 21 3 21 9"></polyline>
                   <line x1="10" y1="14" x2="21" y2="3"></line>
                 </svg>
-                <span class="gantt-chart__btn-text">Карточка</span>
               </button>
               ${dateRangeHtml}
             `;
