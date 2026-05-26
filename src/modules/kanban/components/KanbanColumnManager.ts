4b26476da83bb23236f3caba1eec135c9a774637
Nisakoo <modeniki10@gmail.com>
2026-05-27 01:46:42 +0300

    fix: section manage modal

--- src/modules/kanban/components/KanbanColumnManager.ts
+++ src/modules/kanban/components/KanbanColumnManager.ts
@@ -100,7 +100,7 @@ export class KanbanColumnManager {
         </div>
         <div class="manage-columns__actions">
           <button class="icon-btn manage-columns__delete" data-id="${s.id}" data-name="${s.section_name}">
-            <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#ff5c5c" stroke-width="2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>
+            <svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="#ff5c5c" stroke-width="2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>
           </button>
           <div class="manage-columns__color-trigger bg-${s.color}" data-id="${s.id}"></div>
           <div class="manage-columns__drag">≡</div>
